4a9091503896a2455305f355fd883a08a8cd8b80
bajatin <43754300+bajatin@users.noreply.github.com>
2020-09-01 00:10:26 +0530

    Exception for blob links

--- ctft/github_scrape.py
+++ ctft/github_scrape.py
@@ -2,10 +2,16 @@ from bs4 import BeautifulSoup
 from ctft.souper import souper
 import html2text
 from ctft.formatter import formatter
-
+import asyncio
+import re
 async def github_scraper(url):
     soup = await souper(url)
     article = soup.find("article")
     if article:
         markdwn = formatter(article)
+        return markdwn
+    if re.search("blob",url):
+        c = '<a href="{}" >Original writeup</a>'.format(url)
+        soup2 = BeautifulSoup(c,'html.parser')
+        markdwn = formatter(soup2)
         return markdwn
